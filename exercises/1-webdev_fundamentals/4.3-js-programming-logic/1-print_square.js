0e3a640fecebc7ec05e2b36c24111c6600570d65
Renato Feitosa <pythaqua@gmail.com>
2020-11-30 00:03:25 -0300

    add exercise #1

---
+++ exercises/1-webdev_fundamentals/4.3-js-programming-logic/1-print_square.js
@@ -0,0 +1,38 @@
+/* 
+
+EXERCÍCIO:
+1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo: 
+
+n = 5
+
+*****
+*****
+*****
+*****
+*****
+
+*/
+
+const star = '*';
+const squareSize = 10;
+let line; // Sets the line to be writen
+
+// First solution: using loops and printing data in every round
+for (let row = 0; row < squareSize; row += 1) {
+    line = star.repeat(squareSize);
+    console.log(line);
+}
+
+// // Second solution: using an array and separating data processing from output
+// let squareArray = [];
+// for (let row = 0; row < squareSize; row += 1) {
+//     line = star.repeat(squareSize);
+//     squareArray.push(line);
+// }
+// console.log(squareArray.join('\n'));
+
+// // Third and simpliest solution:
+// const line = star.repeat(squareSize);
+// // Makes a string with a newline char after stars and repeats it
+// const square = (line + '\n').repeat(squareSize); 
+// console.log(square);
