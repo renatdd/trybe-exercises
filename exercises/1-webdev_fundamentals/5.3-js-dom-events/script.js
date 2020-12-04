d8c91cb50b361c59dd85b1e53320b67aae3e86c9
Renato Feitosa <pythaqua@gmail.com>
2020-12-04 19:32:34 -0300

    exercise 1 - createDaysOfTheMonth

--- exercises/1-webdev_fundamentals/5.3-js-dom-events/script.js
+++ exercises/1-webdev_fundamentals/5.3-js-dom-events/script.js
@@ -1,14 +1,3 @@
-/* 
-Exercício 1:
-O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days" . Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Segunda-feira e Terça-feira.
-
-Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . Ex: <li class="day">3</li>
-Os dias 24, 25 e 31 são feriados e, além da classe day , devem conter também a classe holiday . Ex: <li class="day holiday">24</li>
-Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . Ex: <li class="day friday">4</li>
-
-*/
-
-const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
 
 function createDaysOfTheWeek() {
   const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
@@ -25,4 +14,33 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-// Escreva seu código abaixo.
+/* 
+Exercício 1:
+O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days" . Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Segunda-feira e Terça-feira.
+
+Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . Ex: <li class="day">3</li>
+Os dias 24, 25 e 31 são feriados e, além da classe day , devem conter também a classe holiday . Ex: <li class="day holiday">24</li>
+Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . Ex: <li class="day friday">4</li>
+*/
+function createDaysOfTheMonth() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const daysElementList = document.querySelector('#days');
+  const holidays = [24, 25, 31];
+  const fridays = [4, 11, 18, 25];
+  for (let index in dezDaysList) {
+    let dayNumber = dezDaysList[index];
+    let dayLiElement = document.createElement('li');
+    dayLiElement.textContent = dayNumber;
+    let dayClasses = 'day';
+    if (holidays.includes(dayNumber)) {
+      dayClasses += ' holiday';
+    }
+    if (fridays.includes(dayNumber)) {
+      dayClasses += ' friday';
+    }
+    dayLiElement.className = dayClasses;
+    daysElementList.appendChild(dayLiElement);
+  }
+}
+
+createDaysOfTheMonth();
