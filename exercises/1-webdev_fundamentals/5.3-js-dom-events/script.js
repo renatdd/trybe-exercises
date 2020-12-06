e4f277582ebd700b8d2d3e6ab65e045bb5f3f52e
Renato Feitosa <pythaqua@gmail.com>
2020-12-06 05:38:14 -0300

    5.3 - exercise 4

--- exercises/1-webdev_fundamentals/5.3-js-dom-events/script.js
+++ exercises/1-webdev_fundamentals/5.3-js-dom-events/script.js
@@ -89,3 +89,11 @@ function holidayBtnPress() {
 }
 
 holidayBtnPress();
+
+/* 
+Exercício 4:
+Implemente uma função que receba como parâmetro a string "Sexta-feira" e crie dinamicamente um botão com o nome "Sexta-feira".
+Adicione a este botão o ID "btn-friday" .
+Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
+*/
+createBtn('Sexta-feira');
