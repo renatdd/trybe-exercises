947df744da32817217d12a9abcff0272c0f77fd4
Renato Feitosa <pythaqua@gmail.com>
2020-12-06 05:39:42 -0300

    5.3 - exercise 9

--- exercises/1-webdev_fundamentals/5.3-js-dom-events/script.js
+++ exercises/1-webdev_fundamentals/5.3-js-dom-events/script.js
@@ -174,3 +174,23 @@ function addLabelToTask(labelColor) {
 
 addLabelToTask('red');
 
+/* 
+Exercício 9:
+Implemente uma função que adiciona um evento que ao clicar no elemento com a tag <div> referente a cor da sua tarefa, atribua a este elemento a classe task selected , ou seja, quando sua tarefa possuir a classe task selected ela estará selecionada.
+Ao clicar novamente no elemento a sua classe deverá voltar a ser somente task , ou seja, esta tarefa está deixando de ser uma tarefa selecionada.
+*/
+function tagTaskSelected() {
+  if (event.target.className.includes('selected')) {
+    event.target.classList.remove('selected');
+  } else {
+    event.target.classList.add('selected');
+  }
+}
+
+function selectTask() {
+  const myTasksDiv = document.querySelector('.my-tasks');
+  myTasksDiv.addEventListener('click', tagTaskSelected);
+}
+
+selectTask();
+
