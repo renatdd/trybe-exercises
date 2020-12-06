6b3b8dbbb926403381e4841265bc6c465b4afad6
Renato Feitosa <pythaqua@gmail.com>
2020-12-06 05:39:55 -0300

    5.3 - exercise 10

--- exercises/1-webdev_fundamentals/5.3-js-dom-events/script.js
+++ exercises/1-webdev_fundamentals/5.3-js-dom-events/script.js
@@ -194,3 +194,24 @@ function selectTask() {
 
 selectTask();
 
+/* 
+Exercício 10:
+Implemente uma função que adiciona um evento que ao clicar em um dia do mês no calendário, atribua a este dia a cor da legenda da sua tarefa selecionada.
+Ao clicar novamente no dia com a cor da legenda, a sua cor deverá voltar à configuração inicial rgb(119,119,119) .
+*/
+function selectTaskDay() {
+  let taskColor = document.querySelector('.selected').style.backgroundColor;
+  let dayBgValue = event.target.style.backgroundColor;
+  if (dayBgValue !== '') {
+    taskColor = '';
+  }
+  event.target.style.backgroundColor = taskColor;
+}
+
+function addTaskToDay() {
+  const daysUl = document.querySelector('#days');
+  daysUl.addEventListener('click', selectTaskDay);
+}
+
+addTaskToDay();
+
