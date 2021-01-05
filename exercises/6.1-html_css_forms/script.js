fbd38efba3817f13e6b28f5d536ebf78d6e1c558
Renato Feitosa <pythaqua@gmail.com>
2021-01-05 01:51:29 -0300

    [js] add validateInputDate feature

--- exercises/6.1-html_css_forms/script.js
+++ exercises/6.1-html_css_forms/script.js
@@ -1,3 +1,5 @@
+const initialDateInput = document.querySelector('#initial-date-input');
+const initialDateValidation = document.querySelector('#initial-date-validation');
 const statesSelect = document.querySelector('#state-input');
 const states = {
   'Acre': 'AC',
@@ -47,4 +49,56 @@ function generateStatesOptions() {
   });
 }
 
+const validationPatterns = {
+  dateFormat: input => {
+    return (input.length === 3);
+  },
+  dayValue: input => {
+    return (input[0] > 0 && input[0] <= 31);
+  },
+  monthValue: input => {
+    return (input[1] > 0 && input[1] <= 12);
+  },
+  yearValue: input => {
+    return (input[2] >= 0 && input[2] !== '' && input[2] < 2022);
+  },
+}
+
+const dateValidators = [
+  {
+    validation: validationPatterns.dateFormat,
+    message: 'Data não está no formato dd/mm/aaaa',
+  },
+  {
+    validation: validationPatterns.dayValue,
+    message: 'Valor de dia inválido',
+  },
+  {
+    validation: validationPatterns.monthValue,
+    message: 'Valor de mês inválido',
+  },
+  {
+    validation: validationPatterns.yearValue,
+    message: 'Valor de ano inválido',
+  },
+];
+
+function validateInputDate(event) {
+  initialDateValidation.innerHTML = '';
+  const dateArray = event.target.value.split('/');
+  for (let index = 0; index < dateValidators.length; index += 1) {
+    const validatorObj = dateValidators[index];
+    const validation = validatorObj.validation(dateArray);
+    if (!validation) {
+      initialDateValidation.innerHTML = validatorObj.message;
+      break;
+    }
+  }
+}
+
+function setInitialDateEvent() {
+  initialDateInput.addEventListener('change', validateInputDate);
+}
+
 generateStatesOptions();
+setInitialDateEvent();
