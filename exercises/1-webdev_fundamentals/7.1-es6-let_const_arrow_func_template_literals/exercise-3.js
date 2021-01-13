5fb49e2aa6e04b43108ca219719c2f1d43589e1f
Renato Feitosa <pythaqua@gmail.com>
2021-01-12 21:10:15 -0300

    add exercises 1-3

---
+++ exercises/1-webdev_fundamentals/7.1-es6-let_const_arrow_func_template_literals/exercise-3.js
@@ -0,0 +1,7 @@
+// Crie uma função que receba um número e retorne seu fatorial.
+// Na matemática, o fatorial de um número não negativo N , com a notação N! , é o produto de todos os inteiros menores ou iguais a N . Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+// Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível resolver com uma linha.
+
+const fatorial = num => num === 0 ? 1 : num * fatorial(num - 1);
+
+console.log(fatorial(5));
