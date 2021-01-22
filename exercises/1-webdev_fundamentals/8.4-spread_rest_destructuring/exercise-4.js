28abb5657e9cbb39a5609147a635382f7b47af69
Renato Feitosa <pythaqua@gmail.com>
2021-01-21 23:44:49 -0300

    [feat] add exercise 4

---
+++ exercises/1-webdev_fundamentals/8.4-spread_rest_destructuring/exercise-4.js
@@ -0,0 +1,46 @@
+/*
+Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
+Dica: use object destructuring .
+*/
+
+const assert = require('assert');
+
+const people = [
+  {
+    name: "Nicole",
+    bornIn: 1992,
+    nationality: "Australian"
+  },
+  {
+    name: "Harry",
+    bornIn: 2008,
+    nationality: "Australian"
+  },
+  {
+    name: "Toby",
+    bornIn: 1901,
+    nationality: "Australian"
+  },
+  {
+    name: "Frida",
+    bornIn: 1960,
+    nationality: "Dannish"
+  },
+  {
+    name: "Fernando",
+    bornIn: 2001,
+    nationality: "Brazilian"
+  }
+];
+
+// escreva filterPeople abaixo
+const isAustralian = nationality => nationality === 'Australian';
+const isIn20thCentury = year => year > 1900 && year < 2001;
+const filterPeople = people => people.filter(
+  ({ bornIn, nationality }) => isAustralian(nationality) && isIn20thCentury(bornIn)
+);
+
+const filteredPeople = filterPeople(people);
+
+assert.deepStrictEqual(filteredPeople[0], { name: "Nicole", bornIn: 1992, nationality: "Australian" });
+assert.deepStrictEqual(filteredPeople[1], { name: "Toby", bornIn: 1901, nationality: "Australian" });
