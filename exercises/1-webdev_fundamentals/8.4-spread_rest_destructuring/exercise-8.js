da37a4b7385a18474ba16c1c1ad90a6c6607b853
Renato Feitosa <pythaqua@gmail.com>
2021-01-21 23:45:16 -0300

    [feat] add exercise 8

---
+++ exercises/1-webdev_fundamentals/8.4-spread_rest_destructuring/exercise-8.js
@@ -0,0 +1,13 @@
+/*
+Escreva uma função greet que, dado o nome de uma pessoa, retorna uma mensagem de cumprimento:
+Dica: use default params.
+*/
+
+const assert = require('assert');
+
+// escreva greet abaixo
+const greet = (personName, greeting='Hi') => `${greeting} ${personName}`;
+
+assert.strictEqual(greet("John"), "Hi John");
+assert.strictEqual(greet("John", "Good morning"), "Good morning John");
+assert.strictEqual(greet("Isabela", "Oi"), "Oi Isabela");
