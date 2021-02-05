bc288a544f2654d8de694cb16c0a6a46a34262aa
Renato Feitosa <pythaqua@gmail.com>
2021-02-05 14:42:45 -0300

    [feat] add fixing-functions-1 tests

--- exercises/1-webdev_fundamentals/10.1-jest-first_steps/exercise-1/fixing-functions-1.js
+++ exercises/1-webdev_fundamentals/10.1-jest-first_steps/exercise-1/fixing-functions-1.js
@@ -1,7 +1,3 @@
-// Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes. 
-
-const assert = require('assert');
-
 const greetPeople = (people) => {
   const greeting = 'Hello ';
   const output = [];
@@ -12,7 +8,4 @@ const greetPeople = (people) => {
   return output;
 };
 
-const parameter = ['Irina', 'Ashleigh', 'Elsa'];
-const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
-
-assert.deepStrictEqual(greetPeople(parameter), result);
+module.exports = greetPeople;
