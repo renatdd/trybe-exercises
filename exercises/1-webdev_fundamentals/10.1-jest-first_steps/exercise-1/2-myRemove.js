345baa1baaf8600ffb291c79c57ab94b2e189c76
Renato Feitosa <pythaqua@gmail.com>
2021-02-05 12:10:42 -0300

    [feat] add exercise 2/myRemove tests

--- exercises/1-webdev_fundamentals/10.1-jest-first_steps/exercise-1/2-myRemove.js
+++ exercises/1-webdev_fundamentals/10.1-jest-first_steps/exercise-1/2-myRemove.js
@@ -1,11 +1,3 @@
-// A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
-// Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
-// Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
-// Verifique se o array passado por parâmetro não sofreu alterações
-// Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
-
-const assert = require('assert');
-
 function myRemove(arr, item) {
   let newArr = [];
   for (let i = 0; i < arr.length; i += 1) {
@@ -16,7 +8,4 @@ function myRemove(arr, item) {
   return newArr;
 }
 
-assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
-assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
-assert.deepStrictEqual(myRemove([1, 2, 3, 4]), [1, 2, 3, 4]);
-assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
+module.exports = myRemove;
