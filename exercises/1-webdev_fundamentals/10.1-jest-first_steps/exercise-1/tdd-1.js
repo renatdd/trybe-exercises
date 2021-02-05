91aaf10018454877480461f724bf612b304f198c
Renato Feitosa <pythaqua@gmail.com>
2021-02-05 13:41:40 -0300

    [feat] add tdd-1 tests

--- exercises/1-webdev_fundamentals/10.1-jest-first_steps/exercise-1/tdd-1.js
+++ exercises/1-webdev_fundamentals/10.1-jest-first_steps/exercise-1/tdd-1.js
@@ -1,14 +1,5 @@
 // Escreva a função addOne para passar nos testes já implementados.
 
-const assert = require('assert');
+const addOne = array => array.map(item => item + 1);
 
-const addOne = arr => arr.map(number => number + 1);
-
-const myArray = [31, 57, 12, 5];
-const unchanged = [31, 57, 12, 5];
-const expected = [32, 58, 13, 6];
-const output = addOne(myArray);
-
-assert.strictEqual(typeof addOne, 'function');
-assert.deepStrictEqual(output, expected);
-assert.deepStrictEqual(myArray, unchanged);
+module.exports = addOne;
