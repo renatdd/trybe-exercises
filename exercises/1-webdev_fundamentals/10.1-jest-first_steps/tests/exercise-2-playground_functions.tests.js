1a686bd60cf26e8e61b431e8b8afaec1162b19c8
Renato Feitosa <pythaqua@gmail.com>
2021-02-05 15:20:50 -0300

    [feat] (exercise 2 tests) import functions

---
+++ exercises/1-webdev_fundamentals/10.1-jest-first_steps/tests/exercise-2-playground_functions.tests.js
@@ -0,0 +1,17 @@
+const {
+  calcArea,
+  catAndMouse,
+  compareTrue,
+  concatName,
+  decode,
+  encode,
+  fizzBuzz,
+  footballPoints,
+  generatePhoneNumber,
+  techList,
+  highestCount,
+  hydrate,
+  splitSentence,
+  triangleCheck,
+} = require('../exercise-2-playground_functions');
+
