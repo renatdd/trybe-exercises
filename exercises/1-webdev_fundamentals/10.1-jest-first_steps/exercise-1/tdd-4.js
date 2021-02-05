83538960ddee2f26b2370f91302fc5099e4cf5e9
Renato Feitosa <pythaqua@gmail.com>
2021-02-05 13:57:13 -0300

    [feat] add tdd-4 tests

--- exercises/1-webdev_fundamentals/10.1-jest-first_steps/exercise-1/tdd-4.js
+++ exercises/1-webdev_fundamentals/10.1-jest-first_steps/exercise-1/tdd-4.js
@@ -1,20 +1,5 @@
 // Escreva a função findTheNeedle para passar nos testes já implementados.
 
-const assert = require('assert');
-
 const findTheNeedle = (arr, word) => arr.indexOf(word);
 
-let words = ['house', 'train', 'slide', 'needle', 'book'];
-let expected = 3;
-let output = findTheNeedle(words, 'needle');
-assert.strictEqual(output, expected);
-
-words = ['plant', 'shelf', 'arrow', 'bird'];
-expected = 0;
-output = findTheNeedle(words, 'plant');
-assert.strictEqual(output, expected);
-
-words = ['plant', 'shelf', 'arrow', 'bird'];
-expected = -1;
-output = findTheNeedle(words, 'plat');
-assert.strictEqual(output, expected);
+module.exports = findTheNeedle;
