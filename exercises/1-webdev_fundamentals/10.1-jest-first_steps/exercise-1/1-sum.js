44d6b17b2e749ec671d1da01e862d7ea096a2ac7
Renato Feitosa <pythaqua@gmail.com>
2021-02-05 11:59:42 -0300

    [feat] add exercise 1/sum tests

--- exercises/1-webdev_fundamentals/10.1-jest-first_steps/exercise-1/1-sum.js
+++ exercises/1-webdev_fundamentals/10.1-jest-first_steps/exercise-1/1-sum.js
@@ -1,5 +1,3 @@
-const assert = require('assert');
-
 const checkIfNotNumber = value => typeof(value) !== 'number';
 
 const sum = (number1, number2) => {
@@ -9,16 +7,4 @@ const sum = (number1, number2) => {
   return number1 + number2;
 };
 
-assert.strictEqual(typeof(sum), 'function', 'Sum function must be defined');
-assert.strictEqual(sum(4, 5), 9);
-assert.strictEqual(sum(0, 0), 0);
-assert.strictEqual(typeof(checkIfNotNumber), 'function', 'checkIfNotNumber function must be defined');
-assert.strictEqual(checkIfNotNumber(0), false);
-assert.strictEqual(checkIfNotNumber('0'), true);
-assert.throws(() => {
-    sum(4, '5');
-    sum([], '5');
-    sum({}, 5);
-  }, 
-  Error('Parameters must be numbers.')
-);
+module.exports = sum;
