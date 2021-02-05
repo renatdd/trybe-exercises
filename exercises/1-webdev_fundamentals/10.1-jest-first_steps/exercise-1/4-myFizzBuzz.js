d89be185b69b9d64e47627ee57053fd3ae38a2c9
Renato Feitosa <pythaqua@gmail.com>
2021-02-05 12:40:07 -0300

    [feat] add exercise 4/myFizzBuzz tests

--- exercises/1-webdev_fundamentals/10.1-jest-first_steps/exercise-1/4-myFizzBuzz.js
+++ exercises/1-webdev_fundamentals/10.1-jest-first_steps/exercise-1/4-myFizzBuzz.js
@@ -1,12 +1,3 @@
-// A função myFizzBuzz(num) recebe um número num e retorna "fizzbuzz" se o número for divisível por 3 e 5 , retorna "fizz" se for divisível apenas por 3 , retorna "buzz" se divisível apenas por 5 , retorna o próprio número caso não seja divisível por 3 ou 5 e retorna false caso num não seja um número
-// Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado
-// Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado
-// Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado
-// Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado
-// Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado
-
-const assert = require('assert');
-
 function myFizzBuzz(num) {
   if (typeof num !== 'number') return false;
   if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
@@ -15,8 +6,4 @@ function myFizzBuzz(num) {
   return num;
 }
 
-assert.strictEqual(myFizzBuzz(15), 'fizzbuzz');
-assert.strictEqual(myFizzBuzz(9), 'fizz');
-assert.strictEqual(myFizzBuzz(10), 'buzz');
-assert.strictEqual(myFizzBuzz(11), 11);
-assert.strictEqual(myFizzBuzz('test'), false);
+module.exports = myFizzBuzz;
