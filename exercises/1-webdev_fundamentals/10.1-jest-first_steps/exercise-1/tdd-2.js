cd214093c007169462935f61f1ab1e2120ad7e2d
Renato Feitosa <pythaqua@gmail.com>
2021-02-05 13:46:17 -0300

    [feat] add tdd-2 tests

--- exercises/1-webdev_fundamentals/10.1-jest-first_steps/exercise-1/tdd-2.js
+++ exercises/1-webdev_fundamentals/10.1-jest-first_steps/exercise-1/tdd-2.js
@@ -1,12 +1,5 @@
 // Escreva a função wordLengths para passar nos testes já implementados.
 
-const assert = require('assert');
-
 const wordLengths = arr => arr.map(word => word.length);
 
-const words = ['sun', 'potato', 'roundabout', 'pizza'];
-const expected = [3, 6, 10, 5];
-
-assert.strictEqual(typeof wordLengths, 'function');
-const output = wordLengths(words);
-assert.deepStrictEqual(output, expected);
+module.exports = wordLengths;
