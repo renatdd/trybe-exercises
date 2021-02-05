b82d75ea45306a016bd441a4478a7f85597ca923
Renato Feitosa <pythaqua@gmail.com>
2021-02-05 12:13:33 -0300

    [refac] (exercise 1/myRemove) add describe block

--- exercises/1-webdev_fundamentals/10.1-jest-first_steps/tests/2-myRemove.test.js
+++ exercises/1-webdev_fundamentals/10.1-jest-first_steps/tests/2-myRemove.test.js
@@ -9,18 +9,20 @@ Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
 
 const myRemove = require('../exercise-1/2-myRemove');
 
-it('myRemove([1, 2, 3, 4], 3) retorna [1, 2, 4]', () => {
-  expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
-});
+describe('myRemove', () => {
+  it('myRemove([1, 2, 3, 4], 3) retorna [1, 2, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
 
-it('myRemove([1, 2, 3, 4], 3) não retorna [1, 2, 3, 4]', () => {
-  expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
-});
+  it('myRemove([1, 2, 3, 4], 3) não retorna [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
 
-it('myRemove([1, 2, 3, 4]) retorna [1, 2, 3, 4]', () => {
-  expect(myRemove([1, 2, 3, 4])).toEqual([1, 2, 3, 4]);
-});
+  it('myRemove([1, 2, 3, 4]) retorna [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4])).toEqual([1, 2, 3, 4]);
+  });
 
-it('myRemove([1, 2, 3, 4], 5) retorna [1, 2, 3, 4]', () => {
-  expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  it('myRemove([1, 2, 3, 4], 5) retorna [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
 });
