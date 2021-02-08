05f75fa92583a8c4f660c4c9ad07bae7f2a34f18
Renato Feitosa <pythaqua@gmail.com>
2021-02-08 19:38:34 -0300

    [feat] add source to exercise 2 and 3

---
+++ exercises/1-webdev_fundamentals/10.2-jest-asynchronous_tests/src-exercises-2-3.js
@@ -0,0 +1,26 @@
+/*
+Código-base para os exercícios 2 e 3:
+O código a seguir simula uma chamada ao banco de dados para buscar usuários.
+O resultado dessa busca é uma Promise, que é utilizada pelo método getUserName .
+*/
+
+const users = {
+  4: { name: 'Mark' },
+  5: { name: 'Paul' }
+};
+
+const findUserById = (id) => {
+  return new Promise((resolve, reject) => {
+    if (users[id]) {
+      return resolve(users[id]);
+    };
+
+    return reject({ error: 'User with ' + id + ' not found.' });
+  });
+};
+
+const getUserName = (userID) => {
+  return findUserById(userID).then(user => user.name);
+};
+
+module.exports = getUserName;
