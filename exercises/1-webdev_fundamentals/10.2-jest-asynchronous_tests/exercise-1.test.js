a925c78c6ff4a41dca95e1e16cc70fbe5a9640e2
Renato Feitosa <pythaqua@gmail.com>
2021-02-08 19:38:05 -0300

    [feat] add exercise 1

---
+++ exercises/1-webdev_fundamentals/10.2-jest-asynchronous_tests/exercise-1.test.js
@@ -0,0 +1,16 @@
+/*
+Escreva um teste que verifique a chamada do callback de uma função uppercase,
+que transforma as letras de uma palavra em letras maiúsculas.
+Lembre-se de ter cuidado com os falso-positivos em testes assíncronos.
+*/
+
+const uppercase = (str, callback) => {
+  callback(str.toUpperCase());
+};
+
+it('should return the string "test" in uppercase', (done) => {
+  uppercase('test', (string) => {
+    expect(string).toBe('TEST');
+    done();
+  });
+});
