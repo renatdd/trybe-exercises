f9f9e2f5f0997d2004935eb7a628b83b092b54da
Renato Feitosa <pythaqua@gmail.com>
2021-02-23 16:24:46 -0300

    [feat] (Pokedex) add navigation buttons

--- exercises/2-front-end/12.1-react-states_and_events/exercise-pokedex-state/src/Pokedex.js
+++ exercises/2-front-end/12.1-react-states_and_events/exercise-pokedex-state/src/Pokedex.js
@@ -3,18 +3,23 @@ import Pokemon from './Pokemon';
 import pokemons from './data';
 
 
-const opa = () => { alert(this); }
-const typeButton = type => <div onClick={opa} className="TypeButton">{type}</div>;
-
 class Pokedex extends React.Component {
   render() {
     const pokemonTypes = pokemons.map(({type}) => type);
     const uniqueTypes = [...new Set(pokemonTypes)];
     return (
       <div className="Pokedex">
-        {pokemons.sort(({id:thisId}, {id:nextId}) => thisId - nextId)
-          .map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)
-        }
+        <nav className="Pokedex-nav">
+          {
+            ['Next', ...uniqueTypes].map(type => <button>{ type }</button>)
+          }
+        </nav>
+        <section className="Pokedex-display">
+          {
+            pokemons.sort(({id:thisId}, {id:nextId}) => thisId - nextId)
+              .map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)
+          }
+        </section>
       </div>
     );
   }
